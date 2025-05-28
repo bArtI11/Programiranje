97ae80d8e8f2a072e3eba14876b763d660056e89
bArtI11 <153268011+bArtI11@users.noreply.github.com>
2025-05-28 13:45:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,68 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include "header.h"
+#include "main.h"
 
 int main() {
-	Proizvod proizvodi[MAX_PROIZVODA];
-	int broj = 0;
-	int izbor;
-	char datoteka[] = "proizvodi.dat";
+    Proizvod *proizvodi = NULL;
+    int brojProizvoda = 0;
+    int izbor;
 
-	do {
-		printf("\n=== IZBORNIK ===\n");
-		printf("1. Unos proizvoda\n");
-		printf("2. Ispis svih proizvoda\n");
-		printf("3. Sortiraj po datumu\n");
-		printf("4. Sortiraj po nazivu\n");
-		printf("5. Ukupno proizvedeno na dan\n");
-		printf("6. Spremi u datoteku\n");
-		printf("7. Ucitaj iz datoteke\n");
-		printf("0. Izlaz\n");
-		printf("Vas izbor: ");
-		scanf("%d", &izbor);
+    ucitajIzDatoteke(&proizvodi, &brojProizvoda);
 
-		switch (izbor) {
-		case 1:
-			if (broj < MAX_PROIZVODA) {
-				proizvodi[broj++] = unesiProizvod();
-			}
-			else {
-				printf("Dosegnut maksimalan broj proizvoda!\n");
-			}
-			break;
-		case 2:
-			ispisiProizvode(proizvodi, broj);
-			break;
-		case 3:
-			qsort(proizvodi, broj, sizeof(Proizvod), usporediPoDatumu);
-			ispisiProizvode(proizvodi, broj);
-			break;
-		case 4:
-			qsort(proizvodi, broj, sizeof(Proizvod), usporediPoNazivu);
-			ispisiProizvode(proizvodi, broj);
-			break;
-		case 5: {
-			Datum d = unesiDatum();
-			int ukupno = ukupnoZaDatum(proizvodi, broj, d);
-			printf("Ukupno proizvedeno %02d.%02d.%d: %d\n", d.dan, d.mjesec, d.godina, ukupno);
-			break;
-		}
-		case 6:
-			spremiUDatoteku(proizvodi, broj, datoteka);
-			break;
-		case 7:
-			broj = ucitajIzDatoteke(proizvodi, datoteka);
-			break;
-		case 0:
-			printf("Izlaz iz programa.\n");
-			break;
-		default:
-			printf("Nepoznata opcija.\n");
-		}
-	} while (izbor != 0);
+    do {
+        printf("\n1. Dodaj\n2. Ispisi\n3. Spremi\n4. Obrisi\n5. Azuriraj\n6. Sortiraj po nazivu\n7. Sortiraj po datumu\n0. Izlaz\nIzbor: ");
+        scanf("%d", &izbor);
+        switch (izbor) {
+            case 1: dodajProizvod(&proizvodi, &brojProizvoda); break;
+            case 2: ispisiProizvode(proizvodi, brojProizvoda); break;
+            case 3: spremiUDatoteku(proizvodi, brojProizvoda); break;
+            case 4: obrisiProizvod(proizvodi, &brojProizvoda); break;
+            case 5: azurirajProizvod(proizvodi, brojProizvoda); break;
+            case 6: sortirajPoNazivu(proizvodi, brojProizvoda); break;
+            case 7: sortirajPoDatumu(proizvodi, brojProizvoda); break;
+        }
+    } while (izbor != 0);
 
-	return 0;
+    free(proizvodi);
+    return 0;
 }
-
-
