0bbb6915a6224dcca79810fb2cc474de11d27737
bArtI11 <153268011+bArtI11@users.noreply.github.com>
2025-07-01 14:13:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,25 +10,27 @@ int main() {
         prikaziIzbornik();
         printf("Izbor: "); scanf("%d", &izbor);
         switch (izbor) {
-            case 1: dodajProizvod(); break;
-            case 2: ispisiProizvode(); break;
-            case 3: azurirajProizvod(); break;
-            case 4: obrisiProizvod(); break;
-            case 5: spremiUDatoteku(); break;
-            case 6: ucitajIzDatoteke(); break;
-            case 7: backupDatoteke(); break;
-            case 8: obrisiDatoteku(); break;
-            case 9: {
-                int n; printf("Koji zapis? "); scanf("%d", &n);
-                ucitajNTiProizvod(n); break;
-            }
-            case 10: prikaziPoziciju(); break;
-            case 11: rewindDatoteke(); break;
-            case 12: sortirajPoNazivu(); break;
-            case 13:
-                printf("Izlaz.\n");
-                break;
-            default: printf("Pogresan unos.\n"); break;
+    case 1: dodajProizvod(); break;
+    case 2: ispisiProizvode(); break;
+    case 3: azurirajProizvod(); break;
+    case 4: obrisiProizvod(); break;
+    case 5: spremiUDatoteku(); break;
+    case 6: ucitajIzDatoteke(); break;
+    case 7: backupDatoteke(); break;
+    case 8: obrisiDatoteku(); break;
+    case 9: {
+        int n;
+        printf("Unesi redni broj: ");
+        scanf("%d", &n);
+        ucitajNTiProizvod(n);
+        break;
+    }
+    case 10: prikaziPoziciju(); break;
+    case 11: rewindDatoteke(); break;
+    case 12: sortirajPoNazivu(); break;
+    case 13: printf("Izlaz.\n"); break;
+    default: printf("Nepostojeca opcija.\n"); break;
+}
         }
     } while (izbor != 13);
 
