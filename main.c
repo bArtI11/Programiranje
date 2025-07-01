19bdace47c99b0e237e9b1387132f6bfaacb40ba
bArtI11 <153268011+bArtI11@users.noreply.github.com>
2025-07-01 13:07:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include "proizvod.h"
 
+// Glavna funkcija programa.
+// Povezuje sve CRUD operacije i dodatne opcije kroz izbornik.
 int main() {
     int izbor;
+
     inicijalizirajListu();
-    ucitajIzDatoteke();
+    ucitajIzDatoteke(); // Na početku odmah učitaj podatke ako postoje
 
     do {
         prikaziIzbornik();
@@ -14,12 +16,12 @@ int main() {
         scanf("%d", &izbor);
 
         switch (izbor) {
-        case 1: dodajProizvod(); break;
-        case 2: ispisiProizvode(); break;
-        case 3: azurirajProizvod(); break;
-        case 4: obrisiProizvod(); break;
-        case 5: spremiUDatoteku(); break;
-        case 6: ucitajIzDatoteke(); break;
+        case 1: dodajProizvod(); break;   // Create
+        case 2: ispisiProizvode(); break; // Read
+        case 3: azurirajProizvod(); break;// Update
+        case 4: obrisiProizvod(); break;  // Delete
+        case 5: spremiUDatoteku(); break; // Spremanje
+        case 6: ucitajIzDatoteke(); break;// Učitavanje
         case 7: sortirajPoNazivu(); break;
         case 8: sortirajPoDatumu(); break;
         case 9: pretraziPoNazivu(); break;
@@ -27,20 +29,16 @@ int main() {
         case 11: statistikaKolicina(); break;
         case 12: prikaziOstatkeNaDan(); break;
         case 13:
-            printf("Zelite li spremiti podatke prije izlaska? (1 = DA / 0 = NE): ");
-            int potvrda;
-            scanf("%d", &potvrda);
-            if (potvrda == 1) {
-                spremiUDatoteku();
-                printf("Podaci su spremljeni.\n");
-            }
+            printf("Zelite li spremiti prije izlaska? (1 = DA / 0 = NE): ");
+            int potvrda; scanf("%d", &potvrda);
+            if (potvrda) spremiUDatoteku();
             printf("Izlaz iz programa.\n");
             break;
         default: printf("Pogresan izbor!\n"); break;
         }
+
     } while (izbor != 13);
 
-    oslobodiMemoriju();
+    oslobodiMemoriju(); // Oslobađa svu memoriju prije izlaska
     return 0;
 }
-
